5819be5dd35f0c5603633e7d285204e920870c90
FacundoBeloqui <facu.beloqui04@gmail.com>
2024-10-07 15:37:58 -0300

    Terminado el endpoint-GET Pokemons

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Path
 from pydantic import BaseModel
 
 app = FastAPI()
-
-
-class Pokemon_basico(BaseModel):
-    id: int
-    identificador: str
-    imagen: str
-    tipo: str
 
 
 class Pokemon(BaseModel):
@@ -21,42 +14,47 @@ class Pokemon(BaseModel):
     orden: int
     es_default: bool
     imagen: str
-    tipo: str
+    tipo: list[str]
 
 
-lista_nombres_tipos = []
+tipo_nombres = {}
 with open("type_names.csv") as nombres_tipos:
     for linea in nombres_tipos:
         linea = linea.rstrip("\n").split(",")
         if linea[1] == "7":
-            lista_nombres_tipos.append(linea)
-lista_tipos = []
+            tipo_nombres[linea[0]] = linea[2]
+
+pokemon_tipos = {}
 with open("pokemon_types.csv") as tipos:
     for linea in tipos:
         linea = linea.rstrip("\n").split(",")
-        lista_tipos.append(linea)
+        pokemon_id = linea[0]
+        nombre_del_tipo = tipo_nombres.get(linea[1], "")
+        if nombre_del_tipo != "":
+            if linea[0] not in pokemon_tipos:
+                pokemon_tipos[linea[0]] = []
+            pokemon_tipos[linea[0]].append(nombre_del_tipo)
+
 lista_pokemones = []
 with open("pokemon.csv") as pokemones:
-    for contador, linea in enumerate(pokemones):
-        linea = linea.rstrip("\n").split(",")
-        tipo_pokemon = None
-        for tipo in lista_tipos:
-            if tipo[0] == linea[0]:
-                tipo_id = tipo[1]
-                for nombre_tipo in lista_nombres_tipos:
-                    if tipo_id == nombre_tipo[0]:
-                        tipo_pokemon = nombre_tipo[2]
-                        break
-                break
-
-        if tipo_pokemon:
-            pokemon = Pokemon_basico(
-                id=int(linea[0]),
-                identificador=linea[1],
-                imagen=f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{linea[0]}.png",
-                tipo=tipo_pokemon,
-            )
-            lista_pokemones.append(pokemon)
+    for linea in pokemones:
+        linea = linea.rstrip("\n")
+        linea = linea.split(",")
+        if linea[0] == "id":
+            continue
+        pokemon = Pokemon(
+            id=linea[0],
+            identificador=linea[1],
+            id_especie=linea[2],
+            altura=linea[3],
+            peso=linea[4],
+            experiencia_base=linea[5],
+            orden=linea[6],
+            es_default=linea[7],
+            imagen=f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{linea[0]}.png",
+            tipo=pokemon_tipos.get(linea[0], []),
+        )
+        lista_pokemones.append(pokemon)
 
 
 @app.get("/pokemons")
