9142327559bf1c5dc347b9aa1b7068d2fac9ef6c
federico-fredotovich <ffredotovich@fi.uba.ar>
2024-10-30 19:55:19 -0300

    Creo relacion entre tabla equipo y pokemon

--- alembic/versions/2024_10_24_2223-753cee356db5_tabla_pokemones.py
+++ alembic/versions/2024_10_24_2223-753cee356db5_tabla_pokemones.py
@@ -28,11 +28,11 @@ def upgrade() -> None:
         sa.Column('peso', sa.Integer, nullable=False),
         sa.Column('experiencia_base', sa.Integer, nullable=False),
         sa.Column('imagen', sa.String, nullable=False),
-        sa.Column('tipo', sa.ARRAY(sa.String), nullable=False),
+        sa.Column('tipo', sa.BLOB(sa.String), nullable=False),
         sa.Column('grupo_de_huevo', sa.String, nullable=False),
         sa.Column('estadisticas', sa.JSON, nullable=False),
-        sa.Column('habilidades', sa.ARRAY(sa.String), nullable=True),
-        sa.Column('evoluciones_inmediatas', sa.ARRAY(sa.String), nullable=True),
+        sa.Column('habilidades', sa.BLOB(sa.String), nullable=True),
+        sa.Column('evoluciones_inmediatas', sa.BLOB(sa.String), nullable=True),
     )
 
 
