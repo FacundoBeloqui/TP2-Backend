b325a1c352fd681fffab80537e4ad35d6ab3a2f9
FacundoBeloqui <facu.beloqui04@gmail.com>
2024-11-04 15:41:50 -0300

    Implementadas las seeds

--- main.py
+++ main.py
@@ -7,10 +7,33 @@ from db import lista_naturalezas
 from modelos import Naturaleza
 from database import engine
 import logging
+from database import seed_pokemon, seed_naturaleza, seed_movimiento
+from sqlalchemy import Engine
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def init(db_engine: Engine) -> None:
+    try:
+        with Session(db_engine) as session:
+            # Try to create session to check if DB is awake
+            session.exec(select(1))
+    except Exception as e:
+        logger.error(e)
+        raise e
+
+
+def main() -> None:
+    logger.info("Initializing service")
+    init(engine)
+    logger.info("Service finished initializing")
+    seed_pokemon()
+    seed_movimiento()
+    seed_naturaleza
 
 
 app = FastAPI()
 app.include_router(api_router)
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
+main()
