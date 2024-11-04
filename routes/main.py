51f78dc38c9fcdb300aacf4807d0b6e29b7a44a7
FacundoBeloqui <facu.beloqui04@gmail.com>
2024-11-04 17:03:41 -0300

    Funciona el endpoint de movimientos

--- routes/main.py
+++ routes/main.py
@@ -5,5 +5,5 @@ from routes.teams import teams
 
 api_router = APIRouter()
 api_router.include_router(pokemon.router, prefix="/pokemones")
-# api_router.include_router(movimiento.router, prefix="/movimientos")
+api_router.include_router(movimiento.router, prefix="/movimientos")
 api_router.include_router(teams.router, prefix="/teams")
