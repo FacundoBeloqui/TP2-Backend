1d631942c5b3a6252535c16e5a038e79e1a5104c
federico-fredotovich <ffredotovich@fi.uba.ar>
2024-11-27 16:41:39 -0300

    Arreglo que solo pueda haber un nombre de integrante por equipo

--- routes/main.py
+++ routes/main.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 from routes.pokemones import pokemon
 from routes.movimientos import movimiento
 from routes.teams import teams
+from routes.naturalezas import naturaleza
 
 api_router = APIRouter()
 api_router.include_router(pokemon.router, prefix="/pokemones")
 api_router.include_router(movimiento.router, prefix="/movimientos")
 api_router.include_router(teams.router, prefix="/teams")
+api_router.include_router(naturaleza.router, prefix="/naturalezas")
---
+++ routes/naturalezas/naturaleza.py
@@ -0,0 +1,25 @@
+from fastapi import HTTPException, APIRouter, status
+from database import SessionDep
+from modelos import Naturaleza
+from sqlmodel import select
+
+router = APIRouter()
+
+
+
+@router.get("/")
+def get_naturalezas(session: SessionDep) -> list[Naturaleza]:
+    query = select(Naturaleza)
+    movimientos = session.exec(query)
+    return movimientos
+
+
+@router.get("/{id}")
+def show(session: SessionDep, id: int) -> Naturaleza:
+    naturaleza = session.exec(select(Naturaleza).where(Naturaleza.id == id)).first()
+
+    if naturaleza:
+        return naturaleza
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Naturaleza not found"
+    )
